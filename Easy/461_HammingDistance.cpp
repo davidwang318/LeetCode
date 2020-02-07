ac503dc495023a1680693e07e61e835c81628a07
davidwang318 <davidwang106@gmail.com>
2020-02-07 11:57:08 -0500

    Practice prograss: 111

---
+++ Easy/461_HammingDistance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int hDist = 0;
+        for (int i = 0; i < 32; i++){
+            // hDist += (x % 2 != y % 2);
+            hDist += ((x&1) != (y&1));
+            // x /= 2; y /= 2;
+            x >>= 1; y >>= 1;
+	    if(!(x||y)) break;
+        }
+        return hDist;
+    }
+};
+// Nice using XOR
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int xorbit = x^y, hDist = 0;
+        while(xorbit > 0){
+            hDist += xorbit & 1;
+            xorbit >>= 1;
+        }
+        return hDist;
+    }
+};
+
+
