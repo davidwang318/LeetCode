c841a1b7611cb6b6e5500a0f7910a89c5cfa12bd
davidwang318 <davidwang106@gmail.com>
2020-03-01 00:17:36 -0500

    Review progress: 8

---
+++ Medium/146_Cache.cpp
@@ -0,0 +1,37 @@
+// Hard! Be ware of list & splice
+// Time: 69.95%, Memeory: 62.20%
+class LRUCache {
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        auto getIt = m.find(key);
+        if(getIt == m.end()) return -1;
+        l.splice(l.begin(), l, getIt -> second);
+        return getIt -> second -> second;
+    }
+    
+    void put(int key, int value) {
+        auto putIt = m.find(key);
+        if(putIt != m.end()) l.erase(putIt -> second);
+        l.push_front({key, value});
+        m[key] = l.begin();
+        if(l.size() > cap){
+            m.erase(l.rbegin()->first);
+            l.pop_back();
+        }
+    }
+private:
+    unordered_map<int, list<pair<int, int>>::iterator> m;
+    list<pair<int, int>> l;
+    int cap = 0;
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
