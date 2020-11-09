68a9bb1873d580583c018d1313e9c7b74eac4c0d
davidwang318 <davidwang106@gmail.com>
2020-11-09 10:41:58 -0500

    Fix a bug

--- Hard/41_FirstMissingPositive.cpp
+++ Hard/41_FirstMissingPositive.cpp
@@ -3,7 +3,7 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int len = nums.size();
         for(int i = 0; i < len; i ++){
-            while(nums[i] > 0 && nums[i] <= len && nums[i] != i+1){
+            while(nums[i] > 0 && nums[i] <= len && nums[i] != nums[nums[i]-1]){
                 swap(nums[i], nums[nums[i]-1]);
             }
         }
